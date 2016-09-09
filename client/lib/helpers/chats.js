de4dc97ab7f01a6698feffdd5ff59101171e37d0
Diego Lirio <diegolirio.dl@gmail.com>
2016-09-09 13:30:55 -0300

    Felipe Insert Chat - OK

--- client/lib/helpers/chats.js
+++ client/lib/helpers/chats.js
@@ -1,7 +1,15 @@
 Template.chats.helpers({
 
     chats : function() {
-        return [{"id": 1, "descricao": "TDV Friends"}];
+        return Chats.find({});
     }
 
 });
+
+Template.chats.events({
+	"click button": function(e, template){
+		var a = prompt("Informe o nome do Chat");
+		console.log(a);
+		Chats.insert({descricao: a});
+	}
+})
